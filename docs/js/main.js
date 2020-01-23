ec2846f7e7ffef8dd1939c9050e7b430fdd61afc
KatyGlad <konyashkina_e@mail.ru>
2020-01-23 21:18:08 +0300

    Chenges in main.css

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        loop: true,
-        dotsSpeed:900
-    });
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    dotsSpeed: 900
   });
+});
+
+
+
+
+const menuToggle = document.querySelector("#menu-togle");
+const mobileNavContainer = document.querySelector("#mobile-nav")
+
+menuToggle.addEventListener('click', function(){
+	menuToggle.classList.toggle('header__burger-active');
+	mobileNavContainer.classList.toggle("mobile-nav--active");
+})
+document.querySelector(".burger-wrapper").addEventListener('click', function(){
+	document.querySelector(".header__burger").classList.toggle("header__burger-active");
+})
